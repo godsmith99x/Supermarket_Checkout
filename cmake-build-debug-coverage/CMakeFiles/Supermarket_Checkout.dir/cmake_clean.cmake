a882c8a9a5ed3789f531bfe01279e16c0c573f71
Joel Godfrey-Smith <jgodfrey-smith@pm.me>
2021-01-15 15:50:55 -0500

    Added coverage configuration.

---
+++ cmake-build-debug-coverage/CMakeFiles/Supermarket_Checkout.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Supermarket_Checkout.dir/src/Checkout.cpp.o"
+  "CMakeFiles/Supermarket_Checkout.dir/src/Checkout_Test.cpp.o"
+  "Supermarket_Checkout"
+  "Supermarket_Checkout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Supermarket_Checkout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
